7590258ecf7a048c5bf826d4847415fa302acefb
Naomi Kangwa <naomi@yawt.digital>
2022-03-02 10:09:31 +0000

    setting up boilerplate, removing unnecessary files

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export class Button extends React.Component {
+
+}
---
+++ src/components/ToDoMenu.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export class ToDoMenu extends React.Component {
+
+}
